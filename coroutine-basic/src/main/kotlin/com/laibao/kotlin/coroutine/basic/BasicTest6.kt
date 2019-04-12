1701d03b40c263095f8114c253bedfa47ab1b838
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-04-12 22:54:51 +0800

    add readme.md file info

---
+++ coroutine-basic/src/main/kotlin/com/laibao/kotlin/coroutine/basic/BasicTest6.kt
@@ -0,0 +1,25 @@
+package com.laibao.kotlin.coroutine.basic
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking<Unit>{
+
+    launch {
+        delay(2000L)
+        println("Task from runBlocking")
+    }
+
+
+    coroutineScope {
+        launch {
+            delay(5000L)
+            println("Task from nested launch")
+        }
+
+        delay(1000L)
+        println("Task from coroutine scope") // This line will be printed before nested launch
+
+    }
+
+    println("Coroutine scope is over") // This line is not printed until nested launch completes
+}
