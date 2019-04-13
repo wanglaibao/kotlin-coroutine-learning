3fd6f8814f41516f0a1fc4343543fb70632a9f1a
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-04-13 10:41:23 +0800

    add readme.md file info

---
+++ coroutine-basic/src/main/kotlin/com/laibao/kotlin/coroutine/cancel/CancelTest4.kt
@@ -0,0 +1,26 @@
+package com.laibao.kotlin.coroutine.cancel
+
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking{
+
+    val job = launch {
+        try {
+            repeat(100000) { i ->
+                println("I'm sleeping $i ...")
+                delay(500L)
+            }
+        } finally {
+            println("I'm running finally")
+        }
+    }
+
+    delay(30000L) // delay a bit
+    println("main: I'm tired of waiting!")
+
+    job.cancelAndJoin() // cancels the job and waits for its completion
+    println("main: Now I can quit.")
+}
