cf2a9c5a49c202d9e8d5aa1e5fa8bb67a11694d0
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-04-13 10:12:07 +0800

    add readme.md file info

---
+++ coroutine-basic/src/main/kotlin/com/laibao/kotlin/coroutine/cancel/CancelTest2.kt
@@ -0,0 +1,27 @@
+package com.laibao.kotlin.coroutine.cancel
+
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    //sampleStart
+    val startTime = System.currentTimeMillis()
+
+    val job:Job = launch (Dispatchers.Default){
+        var nextPrintTime = startTime
+        var i = 0
+        while (i <= 5) { // computation loop, just wastes CPU
+            // print a message twice a second
+            if (System.currentTimeMillis() >= nextPrintTime) {
+                println("I'm sleeping ${i++} ...")
+                nextPrintTime += 500L
+            }
+        }
+    }
+
+    delay(1300L) // delay a bit
+    println("main: I'm tired of waiting!")
+    job.cancelAndJoin() // cancels the job and waits for its completion
+    //job.join()
+    println("main: Now I can quit.")
+    //sampleEnd
+}
