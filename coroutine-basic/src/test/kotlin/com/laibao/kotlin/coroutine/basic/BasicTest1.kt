4523beb2097220dd4e9586078875b80589047f69
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-04-18 14:27:44 +0800

    add readme.md file info

--- coroutine-basic/src/test/kotlin/com/laibao/kotlin/coroutine/basic/BasicTest1.kt
+++ coroutine-basic/src/test/kotlin/com/laibao/kotlin/coroutine/basic/BasicTest1.kt
@@ -1,11 +1,11 @@
 package com.laibao.kotlin.coroutine.basic
 
-import junit.framework.Assert
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.system.measureTimeMillis
 
 class BasicTest1 {
 
@@ -65,12 +65,139 @@ class BasicTest1 {
         resList.forEach{ println(it)}
 
         // then
-        Assert.assertEquals(resList, listOf("Hello,", "word!"))
+        assertEquals(resList, listOf("Hello,", "word!"))
     }
 
 
     suspend fun expensiveComputation(res: MutableList<String>) {
         delay(5000L)
         res.add("word!")
+    }
+
+
+
+    @Test
+    fun givenHugeAmountOfCoroutines_whenStartIt_thenShouldExecuteItWithoutOutOfMemory() {
+
+        runBlocking<Unit> {
+            // given
+            val counter = AtomicInteger(0)
+
+            val numberOfCoroutines = 1000_000
+
+            // when
+            val jobs = List(numberOfCoroutines) {
+                launch {
+                    delay(2000L)
+                    counter.incrementAndGet()
+                }
+            }
+            jobs.forEach { it.join() }
+
+            println("the counter value is ${counter.get()}")
+
+            // then
+            assertEquals(counter.get(), numberOfCoroutines)
+        }
+    }
+
+
+    @Test
+    fun givenCancellableJob_whenRequestForCancel_thenShouldQuit() {
+        runBlocking<Unit> {
+            // given
+            val job = launch {
+                while (isActive) {
+                    println("is working")
+                }
+            }
+
+            delay(1300L)
+
+            // when
+            job.cancel()
+
+            // then cancel successfully
+
+        }
+    }
+
+
+    @Test(expected = CancellationException::class)
+    fun givenAsyncAction_whenDeclareTimeout_thenShouldFinishWhenTimedOut() {
+        runBlocking<Unit> {
+            withTimeout(1300L) {
+                repeat(1000) { i ->
+                    println("Some expensive computation $i ...")
+                    delay(500L)
+                }
+            }
+        }
+    }
+
+
+    @Test
+    fun givenHaveTwoExpensiveAction_whenExecuteThemAsync_thenTheyShouldRunConcurrently() {
+        runBlocking<Unit> {
+            val delay = 1000L
+            val time = measureTimeMillis {
+                // given
+                val one = async {
+                    //someExpensiveComputation(delay)
+                    delay(2000)
+                }
+                val two = async {
+                    //someExpensiveComputation(delay)
+                    delay(3000)
+                }
+
+                // when
+                runBlocking {
+                    one.await()
+                    two.await()
+                }
+            }
+
+            println("the time value is $time")
+            // then
+            assertTrue(time < delay * 5)
+        }
+    }
+
+
+    @Test
+    fun givenTwoExpensiveAction_whenExecuteThemLazy_thenTheyShouldNotConcurrently() {
+
+        runBlocking<Unit> {
+            val delay = 1000L
+
+            val time = measureTimeMillis {
+                // given
+                val one = async(start= CoroutineStart.LAZY) {
+                    //someExpensiveComputation(delay)
+                    delay(2000)
+                }
+                val two = async(start = CoroutineStart.LAZY) {
+                    //someExpensiveComputation(delay)
+                    delay(3000)
+                }
+
+
+                one.start()
+                two.start()
+
+                // when
+                runBlocking {
+                    one.await()
+                    two.await()
+                }
+            }
+
+            println("the time value is $time")
+
+            // then
+            //assertTrue(time > delay * 4)
+            assertTrue(time < delay * 4)
+        }
     }
 }
