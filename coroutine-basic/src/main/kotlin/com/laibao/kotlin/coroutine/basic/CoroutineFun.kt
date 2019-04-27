7e5f5e77eb2bbb11836e2379b9d9ea7194d4c252
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-04-27 21:27:45 +0800

    add readme.md file info

--- coroutine-basic/src/main/kotlin/com/laibao/kotlin/coroutine/basic/CoroutineFun.kt
+++ coroutine-basic/src/main/kotlin/com/laibao/kotlin/coroutine/basic/CoroutineFun.kt
@@ -3,9 +3,11 @@ package com.laibao.kotlin.coroutine.basic
 import kotlinx.coroutines.*
 import java.time.Instant
 import java.time.ZoneId
+import kotlin.coroutines.CoroutineContext
 
 
 fun main() {
+
 
     fun getTime(): Instant {
         return Instant.now().atZone(ZoneId.systemDefault()).toInstant()
@@ -34,3 +36,35 @@ fun main() {
         println("Sum: $sum")
     }
 }
+
+class SimpleDataSource {
+
+    suspend fun getTasks() = withContext(Dispatchers.Default) {
+        delay(2018)
+        //Result<Task>(error = NullPointerException())
+        //Result<Task>(value = Task(id = 1,title = "wahaha",description = "我们的祖国是花园"))
+    }
+}
+
+
+open class BasePresenter : CoroutineScope {
+
+    private lateinit var job: Job
+
+    //lateinit var view: IView
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
+
+    /*
+        fun bindView(view: IView) {
+            this.view = view
+            job = Job()
+        }
+    */
+
+    fun unBindView() {
+        job.cancel()
+    }
+}
+
