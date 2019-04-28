2198da7da712dc6d50441aa50ac39e3565a72347
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-04-28 15:12:45 +0800

    add readme.md file info

--- kotlin-coroutine-with-reactor/src/main/kotlin/com/laibao/kotlin/coroutine/reactive/repository/ReactiveRepositories.kt
+++ kotlin-coroutine-with-reactor/src/main/kotlin/com/laibao/kotlin/coroutine/reactive/repository/ReactiveRepositories.kt
@@ -5,16 +5,17 @@ import com.laibao.kotlin.coroutine.reactive.entity.Message
 import com.laibao.kotlin.coroutine.reactive.entity.Person
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Mono
 
 @Repository
 interface ReactivePeopleRepository: ReactiveCrudRepository<Person, String>
 
 @Repository
 interface ReactiveAuditRepository: ReactiveCrudRepository<Audit, String> {
-    fun findByEmail(email: String): reactor.core.publisher.Mono<Audit>
+    fun findByEmail(email: String): Mono<Audit>
 }
 
 @Repository
 interface ReactiveMessageRepository: ReactiveCrudRepository<Message, String> {
-    fun countByMessageDateGreaterThanAndEmail(messageDate: java.time.LocalDateTime, email: String): reactor.core.publisher.Mono<Long>
+    fun countByMessageDateGreaterThanAndEmail(messageDate: java.time.LocalDateTime, email: String): Mono<Long>
 }
