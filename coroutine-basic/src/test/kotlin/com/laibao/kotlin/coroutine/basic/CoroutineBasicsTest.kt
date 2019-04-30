c5a8ebe9139b185ad998d90cfcc72ec818713c88
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-01 01:44:58 +0800

    add readme.md file info

--- coroutine-basic/src/test/kotlin/com/laibao/kotlin/coroutine/basic/CoroutineBasicsTest.kt
+++ coroutine-basic/src/test/kotlin/com/laibao/kotlin/coroutine/basic/CoroutineBasicsTest.kt
@@ -169,4 +169,17 @@ class CoroutineBasicsTest {
 
         println("Coroutine_44444444444444 scope is over") // This line is not printed until nested launch completes
     }
+
+
+    @Test
+    fun testFirstCoroutine() = runBlocking{
+
+        GlobalScope.launch { // launch a new coroutine in background and continue
+            delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+            println("World!") // print after delay
+        }
+        println("Hello,") // main thread continues while coroutine is delayed
+        Thread.sleep(2000L) // block main thread for 2 seconds to keep JVM alive
+
+    }
 }
