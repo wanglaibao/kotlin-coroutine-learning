eab062b33e3854fd8011e575f252ab22af8a50b8
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-01 07:47:23 +0800

    add readme.md file info

--- coroutine-basic/src/test/kotlin/com/laibao/kotlin/coroutine/basic/CoroutineBasicsTest.kt
+++ coroutine-basic/src/test/kotlin/com/laibao/kotlin/coroutine/basic/CoroutineBasicsTest.kt
@@ -224,4 +224,24 @@ class CoroutineBasicsTest {
         job.join() // wait until child coroutine completes
 
     }
+
+    @Test
+    fun testSelfDefinitionScopebuilder() = runBlocking{
+
+        launch {
+            delay(2000L)
+            println("Task from runBlocking")
+        }
+
+        coroutineScope { // Creates a coroutine scope
+            launch {
+                delay(500L)
+                println("Task from nested launch")
+            }
+            delay(100L)
+            println("Task from coroutine scope") // This line will be printed before the nested launch
+        }
+
+        println("Coroutine scope is over") // This line is not printed until the nested launch completes
+    }
 }
