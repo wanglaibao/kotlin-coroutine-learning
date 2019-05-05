f1ebf90f84dfd99eeddf73c5bad0ea11b43ebbf3
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-05 15:03:11 +0800

    add readme.md file info

--- coroutine-basic/src/main/kotlin/com/laibao/kotlin/coroutine/context/context01.kt
+++ coroutine-basic/src/main/kotlin/com/laibao/kotlin/coroutine/context/context01.kt
@@ -6,27 +6,27 @@ fun main() = runBlocking<Unit> {
 
 //sampleStart
     launch { // context of the parent, main runBlocking coroutine
-        println("main runBlocking      : I'm working in thread ${Thread.currentThread().name}")
+        println(" 1111111111  main runBlocking : I'm working in thread ${Thread.currentThread().name}")
     }
 
     launch(Dispatchers.Unconfined) { // not confined -- will work with main thread
-        println("Unconfined            : I'm working in thread ${Thread.currentThread().name}")
+        println(" 222222222 Unconfined : I'm working in thread ${Thread.currentThread().name}")
     }
 
     launch(Dispatchers.Default) { // will get dispatched to DefaultDispatcher
-        println("Default               : I'm working in thread ${Thread.currentThread().name}")
+        println(" 333333333 Default : I'm working in thread ${Thread.currentThread().name}")
     }
 
     launch(Dispatchers.IO) { // will get dispatched to DefaultDispatcher
-        println("IO               : I'm working in thread ${Thread.currentThread().name}")
+        println(" 44444444 IO : I'm working in thread ${Thread.currentThread().name}")
     }
 
     launch(newSingleThreadContext("MyOwnThread")) { // will get its own new thread
-        println("newSingleThreadContext: I'm working in thread ${Thread.currentThread().name}")
+        println(" 55555555 newSingleThreadContext: I'm working in thread ${Thread.currentThread().name}")
     }
 
     launch(newFixedThreadPoolContext(5,"MyOwnThread_")) { // will get its own new thread
-        println("newSingleThreadContext: I'm working in thread ${Thread.currentThread().name}")
+        println(" 66666666 newFixedThreadPoolContext: I'm working in thread ${Thread.currentThread().name}")
     }
 
 //sampleEnd
