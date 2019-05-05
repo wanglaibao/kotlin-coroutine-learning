4498fd4a533bee500f17ee71e0aed141bdca2061
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-05 21:43:48 +0800

    add readme.md file info

---
+++ coroutine-basic/src/main/kotlin/com/laibao/kotlin/coroutine/context/context07.kt
@@ -0,0 +1,22 @@
+package com.laibao.kotlin.coroutine.context
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking<Unit> {
+    //sampleStart
+    // launch a coroutine to process some kind of incoming request
+    val request = launch {
+        repeat(3000) { i -> // launch a few children jobs
+            launch  {
+                delay((i + 1) * 200L) // variable delay 200ms, 400ms, 600ms
+                println("Coroutine $i is done")
+            }
+        }
+        println("request: I'm done and I don't explicitly join my children that are still active")
+    }
+    request.join() // wait for completion of the request, including all its children
+    println("Now processing of the request is complete")
+//sampleEnd
+}
