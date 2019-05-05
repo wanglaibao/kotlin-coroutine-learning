0411a4ad73421c172996a9ef285567d7883e4cc9
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-05-05 11:44:18 +0800

    add readme.md file info

--- coroutine-basic/src/test/kotlin/com/laibao/kotlin/coroutine/basic/CancellingCoroutineExecutionTest.kt
+++ coroutine-basic/src/test/kotlin/com/laibao/kotlin/coroutine/basic/CancellingCoroutineExecutionTest.kt
@@ -102,4 +102,52 @@ class CancellingCoroutineExecutionTest {
         job.cancelAndJoin() // cancels the job and waits for its completion
         println("main: Now I can quit.")
     }
+
+    @Test
+    fun testClosingResourcesWithFinally() = runBlocking {
+
+        val job = launch {
+            try {
+                repeat(1000) { i ->
+                    println("I'm sleeping $i ...")
+                    delay(500L)
+                }
+            } finally {
+                println("I'm running finally")
+            }
+        }
+
+        // delay a bit
+        delay(13000L)
+
+        println("main: I'm tired of waiting!")
+
+        // cancels the job and waits for its completion
+        job.cancelAndJoin()
+
+        println("main: Now I can quit.")
+
+    }
+
+    @Test
+    fun testRunNonCancellableBlock() = runBlocking {
+        val job = launch {
+            try {
+                repeat(1000) { i ->
+                    println("I'm sleeping $i ...")
+                    delay(500L)
+                }
+            } finally {
+                withContext(NonCancellable) {
+                    println("I'm running finally")
+                    delay(1000L)
+                    println("And I've just delayed for 1 sec because I'm non-cancellable")
+                }
+            }
+        }
+        delay(1300L) // delay a bit
+        println("main: I'm tired of waiting!")
+        job.cancelAndJoin() // cancels the job and waits for its completion
+        println("main: Now I can quit.")
+    }
 }
